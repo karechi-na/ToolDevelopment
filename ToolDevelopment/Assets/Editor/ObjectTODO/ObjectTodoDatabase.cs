ec63a26feffba75c5fa801914060ffce90006e79
CHI-KUN\chi_k <chi_kun58@icloud.com>
2026-09-01 00:15:22 +0900

    オブジェクトのTODOをインスペクターに表示するようにしました。

--- ToolDevelopment/Assets/Editor/ObjectTODO/ObjectTodoDatabase.cs
+++ ToolDevelopment/Assets/Editor/ObjectTODO/ObjectTodoDatabase.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEditor;
 
@@ -6,8 +7,48 @@ public class ObjectTodoDatabase : ScriptableSingleton<ObjectTodoDatabase>
 {
     public List<ObjectTodoData> todoDataList = new();
 
+    public static event Action DataChanged;
+
     public void SaveDatabase()
     {
         Save(true);
+
+        DataChanged?.Invoke();
     }
+
+	public bool TryDeleteIfCompleted(ObjectTodoData todoData)
+	{
+		if (todoData == null ||
+			todoData.todos == null ||
+			todoData.todos.Count == 0)
+		{
+			return false;
+		}
+
+		bool allCompleted =
+			todoData.todos.TrueForAll(todo => todo.completed);
+
+		if (!allCompleted)
+		{
+			return false;
+		}
+
+		bool delete =
+			EditorUtility.DisplayDialog(
+				"Object TODO",
+				"すべてのTODOが完了しました。\nTODOリストを削除しますか？",
+				"削除",
+				"残す");
+
+		if (!delete)
+		{
+			return false;
+		}
+
+		todoDataList.Remove(todoData);
+
+		SaveDatabase();
+
+		return true;
+	}
 }
